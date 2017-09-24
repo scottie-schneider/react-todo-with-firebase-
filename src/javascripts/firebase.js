db047674c76e12d4761019a4a22f69d87f6787b1
Scottie Schneider <scott@laclave.co>
2017-09-24 18:02:32 -0500

    Updated models for firebase

--- src/javascripts/firebase.js
+++ src/javascripts/firebase.js
@@ -1,5 +1,7 @@
 import * as firebase from 'firebase';
 let database;
+import sectionModel from './models/section'
+import todoModel from './models/todo'
 
 export const init = () => {
   let config = {
@@ -12,4 +14,35 @@ export const init = () => {
   };
   firebase.initializeApp(config);
   database = firebase.database();
+}
+// retrieve from firebase
+// return promise object
+export const getSectionsDB = () => {
+  return database.ref('/').once('value')
+}
+
+// get specified section
+export const getTodoDB = (sectionId) => {
+  return database.ref(`/${sectionId}`).once('value')
+}
+
+// add new section
+export const addSection = (name) => {
+  let key = database.ref('/').push().key
+  let model = sectionModel(key, name, firebase.database.ServerValue.TIMESTAMP)
+  return database.ref('/'+ key).set(model)
+}
+
+// add new todo item into specified section
+export const addTodoItem = (id, name) => {
+  return new Promise((resolve, reject) => {
+    database.ref(`/${id}`).once('value').then((todo) => {
+      let todos = todo.val().todos || []
+      let key = database.ref(`/${id}`).push().key
+      todos.push(todoModel(key, name, firebase.database.ServerValue.TIMESTAMP))
+      database.ref(`/${id}/todos`).set(todos)
+        .then( res => {resolve(res)})
+        .catch( error => {reject(error)})
+    })
+  })
 }
